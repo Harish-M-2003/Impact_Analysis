a3da912b78dc5847e23a84e9729df655258be0b7
Harish M <124487595+Harish-M-2003@users.noreply.github.com>
2026-06-22 20:22:15 +0530

    Delete tests directory

--- tests/test_checkout.py
+++
@@ -1,4 +0,0 @@
-from app.checkout import checkout
-
-def test_checkout():
-    assert checkout(100) == 118
--- tests/test_tax.py
+++
@@ -1,4 +0,0 @@
-from app.tax import calculate_tax
-
-def test_tax():
-    assert calculate_tax(100) == 18
